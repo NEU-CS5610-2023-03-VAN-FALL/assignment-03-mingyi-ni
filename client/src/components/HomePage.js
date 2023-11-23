cfa6e69b79fd6d88b1e2c447e22f8da63c49e4e3
倪鸣一 <592488450@qq.com>
2023-11-22 18:34:49 -0800

    SearchBox.js

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import MovieList from './MovieList';
+import MovieListHeading from "./MovieListHeading";
+import SearchBox from "./SearchBox";
 
 const App = () => {
     const [movies, setMovies] = useState([]);
+    const [searchValue, setSearchValue] = useState('');
 
     const getMovieRequest = async () => {
-        const url = `http://www.omdbapi.com/?s=marvel&apikey=263d22d8`;
+        const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=263d22d8`;
 
         const response = await fetch(url);
         const responseJson = await response.json();
@@ -16,13 +19,17 @@ const App = () => {
     };
 
     useEffect(() => {
-        getMovieRequest();
-    }, []);
+        getMovieRequest(searchValue);
+    }, [searchValue]);
 
     return (
         <div className="movie-app">
+            <div className='row d-flex align-items-center mt-4 mb-4'>
+                <MovieListHeading heading='Search for Movies' />
+                <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+            </div>
             <header>
-                <h1 className="app-title">Marvel Movies</h1>
+                <h1 className="app-title">Movies</h1>
             </header>
             <div className="container">
                 <MovieList movies={movies} />
