8e5bd2ba1d0eae5808deb42d0791aae6c3f0cd79
倪鸣一 <592488450@qq.com>
2023-11-23 18:00:11 -0800

    Basic movie and review adding at communication

--- api/controllers/MovieController.js
+++ api/controllers/MovieController.js
@@ -6,16 +6,37 @@ const prisma = new PrismaClient();
 // Create a new movie
 const createMovie = async (req, res) => {
     try {
-        const { title } = req.body;
-        const movie = await prisma.movie.create({
-            data: { title },
+        const { title, imdbId, /* other movie attributes */ } = req.body;
+
+        // Check if the movie with the same IMDb ID already exists
+        const existingMovie = await prisma.movie.findUnique({
+            where: { imdbId },
         });
-        res.status(201).json(movie);
+
+        if (existingMovie) {
+            // If the movie exists, update its attributes
+            const updatedMovie = await prisma.movie.update({
+                where: { imdbId },
+                data: { title, /* update other attributes */ },
+            });
+
+            res.status(200).json(updatedMovie);
+        } else {
+            console.log(title);
+            // If the movie doesn't exist, create a new one
+            const newMovie = await prisma.movie.create({
+                data: { title, imdbId, /* other movie attributes */ },
+            });
+
+            res.status(201).json(newMovie);
+        }
     } catch (error) {
         console.error(error);
-        res.status(500).json({ error: 'Internal Server Error' });
+        res.status(500).json({ error: 'Internal Server Error'});
     }
 };
+
+
 
 // Get all movies
 const getMovies = async (req, res) => {
@@ -32,6 +53,8 @@ const getMovies = async (req, res) => {
 const getMovieById = async (req, res) => {
     try {
         const { id } = req.params;
+        console.log('Request for Movie ID:', id);
+
         const movie = await prisma.movie.findUnique({
             where: { imdbId: id },
         });
@@ -46,6 +69,7 @@ const getMovieById = async (req, res) => {
         res.status(500).json({ error: 'Internal Server Error' });
     }
 };
+
 
 // Update a movie by ID
 const updateMovie = async (req, res) => {
