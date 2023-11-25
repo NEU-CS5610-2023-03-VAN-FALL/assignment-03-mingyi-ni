282e7a29a3584ee23ba7b18d23a6f986c4ad6342
倪鸣一 <592488450@qq.com>
2023-11-25 15:25:15 -0800

    front end for auth0

--- api/index.js
+++ api/index.js
@@ -1,10 +1,17 @@
 // index.js
+import { auth } from  'express-oauth2-jwt-bearer'
 
 const express = require('express');
 const bodyParser = require('body-parser');
 const movieRoutes = require('./routes/movieRoutes'); // Replace with your actual routes
 const reviewRoutes = require('./routes/reviewRoutes'); // Replace with your actual routes
 const userRoutes = require('./routes/userRoutes'); // Replace with your actual routes
+
+const requireAuth = auth({
+    audience: process.env.AUTH0_AUDIENCE,
+    issuerBaseURL: process.env.AUTH0_ISSUER,
+    tokenSigningAlg: 'RS256'
+});
 
 const app = express();
 const PORT = process.env.PORT || 8000;
@@ -14,6 +21,31 @@ app.use(bodyParser.json());
 
 app.get("/ping", (req, res) => {
     res.send("pong");
+});
+app.post("/verify-user", requireAuth, async (req, res) => {
+    const auth0Id = req.auth.payload.sub;
+    const email = req.auth.payload[`${process.env.AUTH0_AUDIENCE}/email`];
+    const name = req.auth.payload[`${process.env.AUTH0_AUDIENCE}/name`];
+
+    const user = await prisma.user.findUnique({
+        where: {
+            auth0Id,
+        },
+    });
+
+    if (user) {
+        res.json(user);
+    } else {
+        const newUser = await prisma.user.create({
+            data: {
+                email,
+                auth0Id,
+                name,
+            },
+        });
+
+        res.json(newUser);
+    }
 });
 
 // Routes
--- client/src/AppRouter.js
+++ client/src/AppRouter.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
-import LoginPage from './components/LoginPage';
+import VerifyUser from './components/VerifyUser';
 import ProfilePage from './components/ProfilePage';
 import AuthDebuggerPage from './components/AuthDebuggerPage';
 import MovieDetailPage from './components/MovieDetailPage';
@@ -15,7 +15,7 @@ const AppRouter = () => {
             <TopMenu />
             <Routes>
                 <Route path="/" element={<HomePage />} />
-                <Route path="/login" element={<LoginPage />} />
+                <Route path="/verify-user" element={<VerifyUser />} />
                 <Route path="/profile" element={<ProfilePage />} />
                 <Route path="/auth-debugger" element={<AuthDebuggerPage />} />
                 <Route path="/movie/:movieId" element={<MovieDetailPage />} />
